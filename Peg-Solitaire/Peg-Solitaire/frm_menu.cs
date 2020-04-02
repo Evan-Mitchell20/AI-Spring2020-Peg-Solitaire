6e8c05e3904a40328773691e835b344def65c79a
NateSylvia <62341629+NateSylvia@users.noreply.github.com>
2020-04-02 11:57:05 -0400

    Initial Upload of Visual Studio Project and Code Files
    This upload contains the Visual Studio Project and all the starting code files.

---
+++ Peg-Solitaire/Peg-Solitaire/frm_menu.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Peg_Solitaire
+{
+    public partial class frm_menu : Form
+    {
+        public frm_menu()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CenterToScreen();
+
+            // Code below just for debug, will need to be moved
+            GameState theGame = TriangleGames.BasicTriangle(5);
+            //List<List<List<int>>> nextMoves = theGame.nextMoves();
+            //GameState newState = theGame.nextState(nextMoves[0]);
+            //List<GameState> successors = theGame.getSuccessors();
+            DepthFirstAgent depthFirstAgent = new DepthFirstAgent(theGame);
+            BreadthFirstAgent breadthFirstAgent = new BreadthFirstAgent(theGame);
+            List<List<List<int>>> movesToWin = depthFirstAgent.Solve();
+            //List<List<List<int>>> movesToWin = breadthFirstAgent.Solve();
+        }
+    }
+}
